9a5f97df62da1fbfeda18be49e7a7a4145a131c6
SamuelAllan <samuel.allan@gmail.com>
2014-12-06 21:01:31 +0000

    Changed line endings. Removed unused usings.

--- DeployDB/DeployDB.Tests/DeployPlannerTest.cs
+++ DeployDB/DeployDB.Tests/DeployPlannerTest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using DeployDB.Tests.Fakes;
+﻿using DeployDB.Tests.Fakes;
 
 using NUnit.Framework;
 
--- DeployDB/DeployDB.Tests/SqlServer/ScriptSplitterTest.cs
+++ DeployDB/DeployDB.Tests/SqlServer/ScriptSplitterTest.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using DeployDB.SqlServer;
+﻿using DeployDB.SqlServer;
 
 using NUnit.Framework;
 
